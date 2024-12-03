c14da332753463e0b83b30a1b254e73297a447cf
DESTINY WANJIRU <dwngotho@gmail.com>
2024-12-03 10:48:15 +0300

    loop

---
+++ main.py
@@ -0,0 +1,12 @@
+# for i in range(3):
+#     print('Destiny')
+# tableno=int(input('Enter the table no:'))
+# for i in range(1,13,1):
+#  print(i,'*',tableno,'=',i*tableno )
+n=int(input("Enter the no:"))
+sum=0
+i=1
+while(i<=n):
+    sum=sum+i
+    i=i+1
+print('Sum of natural numbers:',sum)
